d21861f8161385a0ad8ba756779f17855e71e6b4
LeeSemi <48410602+LeeSemi@users.noreply.github.com>
2020-03-02 11:25:47 +0900

    Check user id

--- app.js
+++ app.js
@@ -8,6 +8,28 @@ app.get('/users', (req, res)=>{
     return res.json(users);
 });
 
+//[ERROR]::id가 숫자가 아니면
+app.get('/users/:id',(req, res)=>{
+    const id = parseInt(req.params.id, 10);
+    if(!id){
+        return res.status(400).json({err: 'Incorrect id'});
+    }
+    let user = users.filter(user => user.id === id)[0];
+    if(!user) {
+        return res.status(404).json({err: 'Unknown user'});
+    }
+    return res.json(user);
+});
+
+//[ERROR]::없는 id이면
+app.get('/users/:id', (req, res)=>{
+    let user = users.filter(user => user.id === id)[0];
+    if(!user){
+        return res.status(404).json({err: 'Unknown user'});
+    }
+
+});
+
 app.listen(3000,()=>{
     console.log('Example app listening on port 3000!');
 })
